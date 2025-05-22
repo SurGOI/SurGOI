e4da7e8d51bda7d1e54cc9a03a3037fcff18ca6b
SurGOI <j.weerwag1994@outlook.com>
2025-05-22 20:47:09 +0200

    update smoll

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,29 +1,4 @@
-const projects = [
-    {
-        name: "Woodwork",
-        description: "Woodwork is a Minecraft Datapack for 1.21.+",
-        repoUrl: "https://github.com/SurGOI/Woodwork",
-        image: "https://raw.githubusercontent.com/SurGOI/Woodwork/refs/heads/main/readme.png",
-        download: "https://github.com/SurGOI/Woodwork/releases/download/R1.0.0/Woodwork.R1.0.0-1.21.+.zip",
-        downloadEnabled: true
-    },
-    {
-        name: "Scrap Mappy",
-        description: "Scrap Mappy is a Scrap Mechanic Map Previewer.",
-        repoUrl: "https://github.com/SurGOI/ScrapMappy",
-        image: "https://raw.githubusercontent.com/SurGOI/ScrapMappy/refs/heads/main/readme.png",
-        download: "#",
-        downloadEnabled: false
-    },
-    {
-        name: "StationFM",
-        description: "A Personal Radio Station Platform",
-        repoUrl: "https://github.com/SurGOI/StationFM",
-        image: "https://raw.githubusercontent.com/SurGOI/StationFM/refs/heads/main/readme.png",
-        download: "#",
-        downloadEnabled: false
-    }
-];
+import projects from './projects.js';
 
 function loadProjects() {
     const container = document.getElementById("projects");
@@ -33,18 +8,18 @@ function loadProjects() {
         const card = document.createElement("div");
         card.className = "project-card";
 
-        // Try loading the image from the repo
         const img = document.createElement("img");
         img.src = project.image;
         img.alt = `${project.name} image`;
-        img.onerror = () => img.style.display = "none"; // Hide image if not found
+        img.onerror = () => img.style.display = "none";
 
         card.innerHTML = `
             <h3>${project.name}</h3>
             <p>${project.description}</p>
             <div class="buttons">
-                <a href="${project.repoUrl}" class="btn">Read More</a>
-                ${project.downloadEnabled ? `<a href="${project.download}" class="btn">Download</a>` : ""}
+                ${project.sourceEnabled ? `<a href="${project.repoUrl}" target="_blank" class="btn">Source</a>` : ""}
+                ${project.downloadEnabled ? `<a href="${project.download}" target="_blank" class="btn">Download</a>` : ""}
+                ${project.previewEnabled ? `<a href="${project.previewUrl}" target="_blank" class="btn">Live Preview</a>` : ""}
             </div>
         `;
 
@@ -52,6 +27,8 @@ function loadProjects() {
         container.appendChild(card);
     });
 }
+
+document.addEventListener("DOMContentLoaded", loadProjects);
 
 function searchProjects() {
     const query = document.getElementById("search").value.toLowerCase();
